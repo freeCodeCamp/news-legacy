1ab98e80d44339aa633bff90a388ce9769c1e1ec
Stuart Taylor <staylor1012@gmail.com>
2017-11-24 11:54:54 +0000

    ensure safe slugs

---
+++ slugSafe.js
@@ -0,0 +1,35 @@
+const slugg = require('slugg');
+const fse = require('fs-extra');
+const chalk = require('chalk');
+
+function output(str = '', colour = 'blue') {
+  console.log(chalk[colour](str));
+}
+function formatLog(str = '', colour = 'blue') {
+  return chalk[colour](str);
+}
+const pagesPath = './src/pages';
+const safeNameMap = fse.readdirSync(pagesPath, 'utf8')
+  .filter(name => !(/\.jsx?$/).test(name))
+  .reduce((accu, current) => ({
+    ...accu,
+    [current]: `${slugg(current.replace('.md', '').split('-').join(' '))}.md`
+  }), {});
+
+Object.keys(safeNameMap)
+  .forEach(fileName => {
+    const oldName = `${pagesPath}/${fileName}`;
+    const newName = `${pagesPath}/${safeNameMap[fileName]}`;
+    if (oldName !== newName) {
+      output(`
+We have detected a possible unsafe url and corrected it:
+
+It was - ${formatLog(oldName)}
+
+It is now - ${formatLog(newName, 'green')}
+
+Please enusre to commit these changes as they could break the build.
+`, 'yellow');
+    }
+    fse.rename(oldName, newName);
+  });
